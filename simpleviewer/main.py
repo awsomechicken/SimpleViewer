a1570a4ec88833a7c44608328876e7f6fecf2944
Rowdy S <rowdysanford@gmail.com>
2019-12-08 03:16:34 -0800

    make more robust

--- simpleviewer/main.py
+++ simpleviewer/main.py
@@ -188,10 +188,15 @@ def play_video():
     height = config['SCREEN CONF']['Height']
     video_to_play = config['SCREEN CONF']['current video']
     print("File to play:", video_to_play)
-    #try: # start the player
-    player = OMXPlayer(video_to_play, args=['--win', '0 0 %s %s'%(width, height), '--loop'])
-    #except Exception as e:
-    #    print('play_video, omxplayer:', e)
+    try: # start the player
+        player = OMXPlayer(video_to_play, args=['--win', '0 0 %s %s'%(width, height), '--loop'])
+    except Exception as e:
+        print('play_video, omxplayer:', e)
+        # quirie the server
+        thereIsNewVid, filename = check_for_new()
+        if thereIsNewVid:
+            done = get_video(filename) # get the new video
+
 
     while True:
         thereIsNewVid, filename = check_for_new()
