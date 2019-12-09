46521357efb085927455e607fea4d8d84dbec14a
Rowdy S <rowdysanford@gmail.com>
2019-12-08 16:16:30 -0800

    merp?

--- simpleviewer/startup.sh
+++ simpleviewer/startup.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-cd /home/pi/SimpleViewer/simpleviewer/
-
-nohup python3 ./main.py /home/pi/SimpleViewer/simpleviewer/ > /home/pi/simpleviewer.out 2>/home/pi/simpleviewer.err < /dev/null &
+nohup python3 /home/pi/SimpleViewer/simpleviewer/main.py /home/pi/SimpleViewer/simpleviewer/ > /home/pi/simpleviewer.out 2> /home/pi/simpleviewer.err < /dev/null &
